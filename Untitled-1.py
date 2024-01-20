dcb2fd697b3ccb429546255beb08181e01f3d7c5
Made-of-Dark-Matter <divyanshdec14@gmail.com>
2024-01-20 14:43:51 +0530

    Merging main with feature1

--- Untitled-1.py
+++ Untitled-1.py
@@ -39,11 +39,8 @@ Branch commands -
     git checkout <branch_name> [to navigate]
     git checkout -b <new branch name> [to create new branch]
     git branch -f <branch name> [to delete branch]
-<<<<<<< HEAD
-=======
     
 
->>>>>>> e3e1485 (update untitles)
     
 Mergin' Code -
     git diff <branch_name> [to compare differences in commits, files etc]
